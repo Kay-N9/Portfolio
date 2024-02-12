173a5185bbe8f05ba89672c576e8633ed93f5af1
kay-n9 <kadidiaN@hotmail.com>
2024-02-12 18:36:49 +0100

    redesign

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,58 @@
-const head = document.querySelector('.head');
-const puces = document.querySelectorAll('.puce');
-const titre = document.querySelector('.titre');
-const test = document.querySelector('.test');
+const puces = document.querySelectorAll('.puce'); //Selection des nav
 
-window.addEventListener('load', () =>{
+//La fonction vas faire apparaitre mes puces à la suite à l'acctualisation.
+window.addEventListener("load", () =>{
+
     const TL = gsap.timeline({paused: true});
   
     TL
-    .from(titre, 0.9, {height: 10, opacity: 0, ease: "power2.out"})
     .staggerFrom(puces, 0.8, {opacity: 0, ease: "power2.out"}, 0.3)
 
     TL.play();
-  })
+});
+
+
+window.onload = function() {
+    const sections = document.querySelectorAll("section"); //Selection des section
+    const navA = document.querySelectorAll(".puce"); //Selection des puces
+  
+    function updateActiveSection() {
+        // On initialise les deux variable qui vont nous servire de receptacle pour les section et la puces.
+      let maxVisibleArea = 0;
+      let activeSection = null;
+  
+      sections.forEach((section) => {
+          const visibleArea = getVisibleArea(section);
+          
+          if (visibleArea > maxVisibleArea) {
+              maxVisibleArea = visibleArea;
+              activeSection = section.getAttribute("id");
+              console.log(activeSection);
+              section.classList.add("fadeIn");
+            }else{
+                section.classList.remove("fadeIn");
+
+            }
+        });
+        
+        navA.forEach((a) => {
+            a.classList.remove("active");
+            if (a.classList.contains(activeSection)) {
+              a.classList.add("active");
+        }
+      });
+    }
+  
+    function getVisibleArea(element) {
+      const rect = element.getBoundingClientRect();
+      const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+      const windowWidth = window.innerWidth || document.documentElement.clientWidth;
+      const visibleHeight = Math.min(rect.bottom, windowHeight) - Math.max(rect.top, 0);
+      const visibleWidth = Math.min(rect.right, windowWidth) - Math.max(rect.left, 0);
+      return visibleHeight * visibleWidth;
+    }
+  
+    window.addEventListener("scroll", updateActiveSection);
+    window.addEventListener("resize", updateActiveSection);
+  };
+  // Rest of the code
